e37f74e8de26e0905a0528b87ae8f4d19c0b43a1
PrajwalMasulkar <prajwalmasulkar75@gmail.com>
2020-03-25 21:35:32 +0530

    'new'

---
+++ if_else.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int i;
+	cout<<"Enter your no"<<endl;
+	cin>>i;
+	if(i>10)
+		cout<<"Greater than 10";
+	else 
+		cout<<"Smaller than 10";
+	return 0;	
+}
